6596aa64f1e9347c09a192491d84f8db04d6a294
Shivakumar yadav <shivayadavjabbu@gmail.com>
2026-02-13 20:00:17 +0530

    Enhancement: Make MySQL initialization dynamic using init.sh

--- mysql/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS carecircle_auth_service;
-CREATE DATABASE IF NOT EXISTS carecircle_user_profile;
-CREATE DATABASE IF NOT EXISTS carecircle_communication_service;
-CREATE DATABASE IF NOT EXISTS carecircle_matching_booking;
-
--- Force create/update user and grant all permissions
-CREATE USER IF NOT EXISTS 'shiva'@'%' IDENTIFIED WITH mysql_native_password BY 'Shiva@123';
-GRANT ALL PRIVILEGES ON *.* TO 'shiva'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
